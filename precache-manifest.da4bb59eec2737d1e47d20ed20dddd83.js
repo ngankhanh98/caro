1971691a85c04521c7c3ffcc47db5b247e0decf9
Ngan Khanh Nguyen T <ngankhanh98@gmail.com>
2019-09-24 11:40:30 +0700

    Updates

---
+++ precache-manifest.da4bb59eec2737d1e47d20ed20dddd83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b2e4034fa708c028357b1d4e755f36",
+    "url": "/caro/index.html"
+  },
+  {
+    "revision": "e6f4e39cd5e5d299e145",
+    "url": "/caro/static/css/main.03d4a463.chunk.css"
+  },
+  {
+    "revision": "3df0b5e1b014d4933a5b",
+    "url": "/caro/static/js/2.1a0b7844.chunk.js"
+  },
+  {
+    "revision": "e6f4e39cd5e5d299e145",
+    "url": "/caro/static/js/main.e63a329d.chunk.js"
+  },
+  {
+    "revision": "188e0d020c8133dfce68",
+    "url": "/caro/static/js/runtime-main.bea84211.js"
+  }
+]);
